35de9ccd0c38f43a2cc486023407b7e8a26079a4
Jose <josefbautista94@gmail.com>
2025-03-12 16:04:23 -0400

    making progress still missing a few things

--- index.js
+++ index.js
@@ -11,32 +11,48 @@ mainEl.style.justifyContent = "center"; // Centers content horizontally
 mainEl.style.alignItems = "center"; // Centers content vertically
 mainEl.style.flexDirection = "column"; // Stacks items vertically
 
-let bttn = document.createElement("BUTTON");
+// Create the button
+let bttn = document.createElement("button");
 bttn.style.padding = "5em 10em";
 bttn.style.background = "rgb(197, 130, 58)";
 bttn.style.borderRadius = "50px"; // Rounded corners
-bttn.innerHTML = "<h1>Start<br> Game</h1>"; // giving mainEl a h1
+bttn.innerText = "Start Game"; // Fixed innerHTML issue
 bttn.style.color = "white";
 bttn.style.border = "10px solid rgb(200, 195, 209)";
 bttn.onclick = startGame; // This will call startGame() when clicked
 
 mainEl.appendChild(bttn);
 
-let words = ["apples", "water", "elephant","racecar","drums","phone"];
-let original = [...words]
+// Word list
+let words = ["apples", "water", "elephant", "racecar", "drums", "phone"];
 
-function scrambleWords (word){
-let split = ""
+// Function to scramble words (Fisher-Yates Shuffle)
+function scrambleWords(word) {
+    let split = word.split(""); // Convert the word into an array of characters
+    for (let i = split.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1)); // Pick a random index
+        [split[i], split[j]] = [split[j], split[i]]; // Swap characters
+    }
+    return split.join(""); // Convert back to string
 }
+// Function to scramble words using Fisher-Yates Shuffle
+// Source: GeeksforGeeks - "Shuffle a Given Array Using Fisher-Yates Shuffle Algorithm"
+// URL: https://www.geeksforgeeks.org/shuffle-a-given-array-using-fisher-yates-shuffle-algorithm
 
+console.log(scrambleWords(words[Math.floor(Math.random() * words.length)])); // Test scramble function
+
+// Start game function
 function startGame() {
-    let randomWord = words[Math.floor(Math.random() * words.length)];
-    alert(`Can you get this word right: ${randomWord}`);
+    let randomIndex = Math.floor(Math.random() * words.length);
+    let chosenWord = words[randomIndex]; // Pick a random word
+    let scrambledWord = scrambleWords(chosenWord); // Scramble it
 
-    let userInput = window.prompt("What word do you think it is? 👀");
+    alert(`Can you unscramble this word: ${scrambledWord}`);
+
+    let userInput = window.prompt(`Current Word : ${scrambledWord}, What word do you think it is? 👀`);
 
     if (userInput) { // If the user wrote something
-        if (userInput.toLowerCase() === randomWord) {
+        if (userInput.toLowerCase() === chosenWord) { // Compare with original
             alert("You guessed the word right! ✅");
         } else {
             alert("Wrong Answer!");
@@ -45,4 +61,3 @@ function startGame() {
         alert("You didn’t write anything.");
     }
 }
-
